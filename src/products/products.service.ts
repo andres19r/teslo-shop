aaefa16985c1cc5cd52c58480badf53b4efc4866
Andres Rivero <andres19rivero@gmail.com>
2024-10-19 21:20:35 -0400

    fix(products): use query builder to search a product by slug and title

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -46,10 +46,15 @@ export class ProductsService {
     if (isUUID(term)) {
       product = await this.productRepository.findOneBy({ id: term });
     } else {
-      product = await this.productRepository.findOneBy({ slug: term });
+      const queryBuilder = this.productRepository.createQueryBuilder();
+      product = await queryBuilder
+        .where('title ilike :title or slug = :slug', {
+          title: term,
+          slug: term,
+        })
+        .getOne();
     }
 
-    // const product = await this.productRepository.findOneBy({ id: term });
     if (!product) throw new NotFoundException(`Product with ${term} not found`);
 
     return product;
