1467a4c585fb31a8dc50e426cbe868c9fe0dfc43
Andres Rivero <andres19rivero@gmail.com>
2024-10-19 20:49:19 -0400

    feat(products): search product by uuid or slug

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -11,6 +11,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { validate as isUUID } from 'uuid';
 
 @Injectable()
 export class ProductsService {
@@ -40,10 +41,16 @@ export class ProductsService {
     });
   }
 
-  async findOne(id: string) {
-    const product = await this.productRepository.findOneBy({ id });
-    if (!product)
-      throw new NotFoundException(`Product with id ${id} not found`);
+  async findOne(term: string) {
+    let product: Product;
+    if (isUUID(term)) {
+      product = await this.productRepository.findOneBy({ id: term });
+    } else {
+      product = await this.productRepository.findOneBy({ slug: term });
+    }
+
+    // const product = await this.productRepository.findOneBy({ id: term });
+    if (!product) throw new NotFoundException(`Product with ${term} not found`);
 
     return product;
   }
