130b8eebbd12adbba70589e53b8d8cc78ad143f2
Andres Rivero <andres19rivero@gmail.com>
2024-10-22 00:07:11 -0400

    feat(seed): implement first seed resource to delete all data in db

---
+++ src/seed/seed.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { ProductsService } from 'src/products/products.service';
+
+@Injectable()
+export class SeedService {
+  constructor(private readonly productsService: ProductsService) {}
+
+  async execute() {
+    await this.insertNewProducts();
+    return 'SEED EXECUTED';
+  }
+
+  private async insertNewProducts() {
+    await this.productsService.deleteAllProducts();
+    return true;
+  }
+}
