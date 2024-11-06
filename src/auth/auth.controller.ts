d0005ede3ceaed7e46381c2d2dc79cac791609b9
Andres Rivero <andres19rivero@gmail.com>
2024-11-06 00:58:02 -0400

    poc(auth): implement custom property decorator to get rawHeaders

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -4,6 +4,7 @@ import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
 import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
+import { RawHeaders } from './decorators/raw-headers.decorator';
 
 @Controller('auth')
 export class AuthController {
@@ -21,11 +22,17 @@ export class AuthController {
 
   @Get('private')
   @UseGuards(AuthGuard())
-  testingPrivateRoute(@GetUser() user: User) {
+  testingPrivateRoute(
+    @GetUser() user: User,
+    @GetUser('email') userEmail: string,
+    @RawHeaders() rawHeaders: string[],
+  ) {
     return {
       ok: true,
       message: 'Hello World Private',
       user,
+      userEmail,
+      rawHeaders,
     };
   }
 }
