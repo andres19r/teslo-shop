de29db3226f0f05d39efad90ac2e0fd92b6cfe7a
Andres Rivero <andres19rivero@gmail.com>
2024-11-15 00:08:00 -0400

    feat(auth): implement composed auth decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,12 +2,10 @@ import { Body, Controller, Get, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { AuthGuard } from '@nestjs/passport';
-import { GetUser } from './decorators/get-user.decorator';
 import { User } from './entities/user.entity';
-import { RawHeaders } from './decorators/raw-headers.decorator';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
-import { RoleProtected } from './decorators/role-protected/role-protected.decorator';
 import { ValidRoles } from './interfaces';
+import { Auth, GetUser, RawHeaders, RoleProtected } from './decorators';
 
 @Controller('auth')
 export class AuthController {
@@ -43,6 +41,15 @@ export class AuthController {
   @RoleProtected(ValidRoles.superUser, ValidRoles.admin)
   @UseGuards(AuthGuard(), UserRoleGuard)
   privateRoute2(@GetUser() user: User) {
+    return {
+      ok: true,
+      user,
+    };
+  }
+
+  @Get('private3')
+  @Auth(ValidRoles.admin, ValidRoles.superUser)
+  privateRoute3(@GetUser() user: User) {
     return {
       ok: true,
       user,
