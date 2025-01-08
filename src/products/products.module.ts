0e4898291bdf6e058d26d9bcca3228f8f6554800
Andres Rivero <andres19rivero@gmail.com>
2025-01-07 22:44:00 -0400

    import auth module to protect routes in products and seed controllers

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -4,11 +4,16 @@ import { ProductsController } from './products.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Product, ProductImage } from './entities';
 import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [ProductsService],
-  imports: [ConfigModule, TypeOrmModule.forFeature([Product, ProductImage])],
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([Product, ProductImage]),
+    AuthModule,
+  ],
   exports: [ProductsService, TypeOrmModule],
 })
 export class ProductsModule {}
