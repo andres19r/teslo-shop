70ad296a22682393db915a92f85f292a4abdfda1
Andres Rivero <andres19rivero@gmail.com>
2025-01-09 16:01:20 -0400

    doc: add swagger documentation to auth, files, products & seed controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,7 +6,9 @@ import { User } from './entities/user.entity';
 import { UserRoleGuard } from './guards/user-role/user-role.guard';
 import { ValidRoles } from './interfaces';
 import { Auth, GetUser, RawHeaders, RoleProtected } from './decorators';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
