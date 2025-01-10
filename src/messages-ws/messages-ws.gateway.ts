8868f2127bc23860bc87db53491cf9817082aa0f
Andres Rivero <andres19rivero@gmail.com>
2025-01-10 17:08:35 -0400

    feat(ws): add methods in ws service to register and remove clients

--- src/messages-ws/messages-ws.gateway.ts
+++ src/messages-ws/messages-ws.gateway.ts
@@ -13,10 +13,14 @@ export class MessagesWsGateway
   constructor(private readonly messagesWsService: MessagesWsService) {}
 
   handleConnection(client: Socket) {
-    console.log(`Client connected: ${client.id}`);
+    this.messagesWsService.registerClient(client);
+
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
   }
 
   handleDisconnect(client: Socket) {
-    console.log(`Client disconnected: ${client.id}`);
+    this.messagesWsService.removeClient(client.id);
+
+    console.log({ connected: this.messagesWsService.getConnectedClients() });
   }
 }
--- src/messages-ws/messages-ws.service.ts
+++ src/messages-ws/messages-ws.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Socket } from 'socket.io';
+
+interface ConnectedClients {
+  [id: string]: Socket;
+}
 
 @Injectable()
-export class MessagesWsService {}
+export class MessagesWsService {
+  private connectecClients: ConnectedClients = {};
+
+  registerClient(client: Socket) {
+    this.connectecClients[client.id] = client;
+  }
+
+  removeClient(clientId: string) {
+    delete this.connectecClients[clientId];
+  }
+
+  getConnectedClients(): number {
+    return Object.keys(this.connectecClients).length;
+  }
+}
